ad49295fcb84718123b18c12fa185c5c415c61f8
minhanai <hanashinagakunai@gmail.com>
2024-03-11 21:55:40 +0900

    cors対応2

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # FastAPIのインスタンス作成
 app = FastAPI()
-
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
